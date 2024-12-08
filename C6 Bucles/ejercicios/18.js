96efd9bebc2eb4212e080d9a79b66dbaa563b39a
rgma1995 <rgma1995@outlook.com>
2024-12-07 22:24:38 -0300

    Ejercicio 17 y 18 resuelto del M6

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,13 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  if (a<0 || b<0) return 0;
+  var c = a;
+  var i = 0;
+  for (i=a; i<b; i++) {
+    c = c * (i+1);
+  }
+  return c;
 }
 
 module.exports = productoEntreNúmeros;
